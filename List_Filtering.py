810523f65eb9c1bfd132ca276c64644994c13841
DLamarG <demondg8@gmail.com>
2024-01-17 22:25:08 -0600

    solved with list comprehension

--- List_Filtering.py
+++ List_Filtering.py
@@ -1,10 +1,6 @@
 def filter_list(l):
-    result = []
-    for data in l:
-        if isinstance(data, int):
-            result.append(data)
-    return(result)
-
+    filtered_list = list([x for x in l if isinstance(x, int)])
+    return(filtered_list)
 print(filter_list([1, 2, 6, '456', 2, '43']))
 print(filter_list([1, 2, 467, '456', 2, '0']))
 print(filter_list([1, -456, 456, '456', 2, '43']))
